21d670799625ade41a31a71d07dc11589ae3bf63
Leo Douglas Padilha <leo.douglaspadi@mercadolivre.com.br>
2023-07-12 02:47:27 -0300

    Test newrelic params errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,24 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
     kotlin("jvm") version "1.8.21"
     id("java-gradle-plugin")
     id("maven-publish")
+    id("org.jetbrains.dokka") version "1.8.20"
+    signing
 }
+
+apply(plugin = "signing")
 
 group = "com.orbitasolutions"
-version = "0.0.9-SNAPSHOT"
+version = "0.1.1"
 
 repositories {
-    mavenLocal()
     mavenCentral()
-    flatDir {
-        dirs("libs")
-    }
-}
-
-repositories {
-
 }
 
 dependencies {
-//   implementation("com.orbitasolutions:geleia:0.0.23-SNAPSHOT")
-//    api(files("libs/geleia-jvm-0.0.28-SNAPSHOT.jar"))
-    api(":geleia-jvm-0.0.28-SNAPSHOT")
+    implementation("com.orbitasolutions:geleia:0.1.0")
 
     implementation("org.jetbrains.compose.desktop:desktop:1.2.2")
     implementation("org.jetbrains.compose.desktop:desktop-jvm-macos-arm64:1.2.2")
@@ -39,4 +35,80 @@ gradlePlugin {
             implementationClass = "com.orbitasolutions.plugins.geleia.GeleiaPlugin"
         }
     }
+}
+
+val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
+    val dokkaHtml by tasks.getting(DokkaTask::class)
+
+    dependsOn(dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaHtml.outputDirectory)
+}
+
+tasks {
+    task("publishGprPublicationToGeleiaRepository").outputs.cacheIf { true }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    afterEvaluate {
+        publications {
+            withType<MavenPublication> {
+                updatePom(pom)
+                the<SigningExtension>().sign(this)
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "sonatypeStaging"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials(PasswordCredentials::class)
+        }
+    }
+}
+
+fun updatePom(pom: MavenPom?) {
+    if (pom != null) {
+        with(pom) {
+            val projectGitUrl = "https://github.com/leodouglas/geleia-plugin"
+            name.set(rootProject.name)
+            description.set("A maven plugin to geleia project")
+            url.set(projectGitUrl)
+            inceptionYear.set("2023")
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set("orbitasolutions.com")
+                    name.set("Leo Douglas Padilha")
+                    email.set("leodouglas@gmail.com")
+                    url.set("https://www.orbitasolutions.com")
+                }
+            }
+            issueManagement {
+                system.set("GitHub")
+                url.set("$projectGitUrl/issues")
+            }
+            scm {
+                connection.set("scm:git:$projectGitUrl")
+                developerConnection.set("scm:git:$projectGitUrl")
+                url.set(projectGitUrl)
+            }
+
+        }
+    }
+
+}
+
+signing {
+    sign(configurations.archives.get())
 }
